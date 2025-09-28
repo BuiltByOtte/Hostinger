4959be2014dbd65b169595e0fd09d13fd558b1bf
BuiltByOtte <128637052+BuiltByOtte@users.noreply.github.com>
2025-09-28 21:12:26 +0200

    Add files via upload

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\SettingsProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\CustomPagesServiceProvider::class,
+    \SocialiteProviders\Manager\ServiceProvider::class,
+];
